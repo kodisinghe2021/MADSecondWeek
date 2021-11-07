4d7c32139f8cb94cbcb888dce49a22914807ab77
kodisinghe2021 <fxkodisinghe@gmail.com>
2021-11-08 00:54:16 +0530

    container set

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -6,20 +6,22 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          "Kodisinghe Power",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w400,
-            color: Colors.blueAccent,
-            backgroundColor: Colors.amber,
-            letterSpacing: 3.3,
-            wordSpacing: 5,
-          ),
-          textAlign: TextAlign.center,
-          textScaleFactor: 1,
+        child: Container(
+          width: size.width * 0.8,
+          height: size.height / 2,
+          color: Colors.amberAccent,
+          child: const Center(
+              child: Text(
+            "Hellow world",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              letterSpacing: 3,
+            ),
+          )),
         ),
       ),
     );
